10ae08c7c9eb7f6565ac05f63ceb786d50ce6c53
okwuduche <chinweikeokwuduche@gmail.com>
2023-10-31 16:34:05 +0000

    i18n

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+""" 0. Basic Flask app """
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+def get_locale():
+    """ get_locale """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        print(locale)
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+class Config():
+    """ Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = LANGUAGES[0]
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+    # BABEL_LOCALE_SELECTOR = get_locale
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+# Either define it this like  or with it defined in the
+# config object
+# babel.init_app(app, locale_selector=get_locale)
+babel.init_app(app, locale_selector=get_locale)
+
+
+@app.route('/')
+def index():
+    """ index """
+    return render_template("4-index.html", get_locale=get_locale)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
---
+++ 0x02-i18n/app.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+"""
+Flask app
+"""
+import locale
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g
+)
+from flask_babel import Babel
+from datetime import timezone as tmzn
+from datetime import datetime
+from pytz import timezone
+import pytz.exceptions
+from typing import (
+    Dict,
+    Union
+)
+from flask_babel import gettext as _
+
+
+class Config(object):
+    """
+    Configuration for Babel
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """
+    Returns a user dictionary or None if ID value can't be found
+    or if 'login_as' URL parameter was not found
+    """
+    id = request.args.get('login_as', None)
+    if id and int(id) in users.keys():
+        return users.get(int(id))
+    return None
+
+
+@app.before_request
+def before_request():
+    """
+    Add user to flask.g if user is found
+    """
+    user = get_user()
+    g.user = user
+    time_now = pytz.utc.localize(datetime.utcnow())
+    time = time_now.astimezone(timezone(get_timezone()))
+    locale.setlocale(locale.LC_TIME, (get_locale(), 'UTF-8'))
+    fmt = "%b %d, %Y %I:%M:%S %p"
+    g.time = time.strftime(fmt)
+
+
+@babel.localeselector
+def get_locale():
+    """ get_locale """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user.get('locale')
+    locale = request.headers.get('locale')
+    if locale in app.config['LANGUAGES']:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezoneselector
+def get_timezone():
+    """
+    Select and return appropriate timezone
+    """
+    tzone = request.args.get('timezone', None)
+    if tzone:
+        try:
+            return timezone(tzone).zone
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+    if g.user:
+        try:
+            tzone = g.user.get('timezone')
+            return timezone(tzone).zone
+        except pytz.exceptions.UnknownTimeZoneError:
+            pass
+    dflt = app.config['BABEL_DEFAULT_TIMEZONE']
+    return dflt
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('index.html', locale=get_locale)
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
